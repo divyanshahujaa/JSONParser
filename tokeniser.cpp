0b9062329d82f3504481ac2713d8f7e025c900e9
Divyansh <divyansh9801@gmail.com>
2025-12-03 01:08:47 +0530

    Refactored tokeniser and parser

--- tokeniser.cpp
+++ tokeniser.cpp
@@ -1,95 +1,267 @@
 #include <string>
 #include <vector>
-
-enum TokenType{
-    OPENBRACE,
-    CLOSEDBRACE,
-    COLON,
-    KEY,
-    VALUE,
-    DELIMITER
-};
-class JSONToken{
-public:
-    std::string data;
-    TokenType type;
-
-    JSONToken(TokenType type){
-        this->type = type;
-    }
-
-    JSONToken(std::string data, TokenType type){
-        this->data = data;
-        this->type = type;
-    }
-};
+#include <unordered_map>
+#include <iostream>
+#include <variant>
+#include <map>
+#include "tokeniser.h"
 
 
-enum TokeniserState{
-    START,
-    PARSINGKEY,
-    PARSINGVALUE,
-    ParsingKeyInQuotes,
-    ParsingValInQuotes,
-    OUTQUOTES
-};
 
-/*{key : value, key2 : value2, ...}
-ignore spaces, int only parsing*/
-std::vector<JSONToken> tokenise(std::string json){
-    int n = json.length();
-    std::vector<JSONToken> tokenList;
-    TokeniserState state = START;
-    std::string data = "";
-    for(char c: json){
-        switch (c)
+std::map<int, std::string> translator = {{OPENBRACE, "OPENBRACE"}, {CLOSEDBRACE, "CLOSEDBRACE"},
+    {COLON, "COLON"},
+    {STRING, "STRING"},
+    {INTEGER, "INTEGER"},
+    {FLOAT, "FLOAT"},
+    {BOOL, "BOOL"},
+    {NULLOBJ, "NULLOBJ"},
+    {DELIMITER, "DELIMITER"},
+    {INVALID, "INVALID"},
+    {UNKNOWN, "UNKNOWN"}};
+
+
+
+
+Token getToken(FILE* pFile){
+    char currChar;
+    Token t;
+    bool isIgnoreSpaces = true;
+    bool isReadingString = false;
+    bool isPreDecimal = true;
+    int mayBeNull = 0;
+    int mayBeTrue = 0;
+    int mayBeFalse = 0;
+    TokenType currTokenType = UNKNOWN;
+
+    while((currChar = fgetc(pFile))!=EOF){
+        // std::cout<<currChar<<std::endl;
+        switch (currChar)
         {
-        case ' ':
-            
-            break;
-        
         case '{':
-            state = PARSINGKEY;
-            tokenList.push_back(JSONToken(OPENBRACE));
+            if(currTokenType==UNKNOWN){
+                t.type = OPENBRACE;
+                return t;
+            }
+            else if(currTokenType==STRING){
+                t.val = std::get<std::string>(t.val) +currChar;
+            }
+            else{
+                perror("INVALID TOKEN");
+                t.type = INVALID;
+                return t;
+            }
             break;
         case '}':
-            //state = CLOSEBRACKET;
-            tokenList.push_back(JSONToken(CLOSEDBRACE));
-            break;
-        case '"':
-            if(state == PARSINGKEY){
-                state = ParsingKeyInQuotes;
+            if(currTokenType==UNKNOWN){
+                t.type = CLOSEDBRACE;
+                return t;
             }
-            else if(state == PARSINGVALUE){
-                state = ParsingValInQuotes;
+            else if(currTokenType==STRING){
+                t.val = std::get<std::string>(t.val) +currChar;
             }
-            else if(state == ParsingKeyInQuotes){
-                state = PARSINGKEY;
-                tokenList.push_back(JSONToken(data, KEY));
-                data = "";
-            }
-            else if(state == ParsingValInQuotes){
-                state = PARSINGVALUE;
-                tokenList.push_back(JSONToken(data, VALUE));
-                data = "";
+            else{
+                // perror("INVALID TOKEN");
+                // t.type = INVALID;
+                ungetc(currChar, pFile);
+                return t;
             }
             break;
         case ':':
-            state = PARSINGVALUE;
-            tokenList.push_back(JSONToken(COLON));
+            if(currTokenType==UNKNOWN){
+                t.type = COLON;
+            return t;
+            }
+            else if(currTokenType==STRING){
+                t.val = std::get<std::string>(t.val) +currChar;
+            }
+            else{
+                perror("INVALID TOKEN");
+                t.type = INVALID;
+                return t;
+            }
             break;
         case ',':
-            state = PARSINGKEY;
-            tokenList.push_back(JSONToken(DELIMITER));
+            if(currTokenType==UNKNOWN){
+                t.type = DELIMITER;
+                return t;
+                //ungetc()
+            }
+            else if(currTokenType==STRING){
+                t.val = std::get<std::string>(t.val) +currChar;
+            }
+            else{
+                ungetc(currChar, pFile);
+                return t;
+            }
+            
             break;
+        case ' ':
+            if(isIgnoreSpaces){
+                continue;
+            }
+            else{
+                if(currTokenType == STRING){
+                    t.val = std::get<std::string>(t.val) +currChar;
+                }
+                else{
+                    perror("INVALID TOKEN");
+                    t.type = INVALID;
+                    return t;
+                }
+            }
+            break;
+        case '"':
+            if(isReadingString){
+                isReadingString = false;
+                return t;
+            }
+            else{
+                isReadingString = true;
+                currTokenType = STRING;
+                t.type = STRING;
+            }
+            break;
+        case '.':
+            if(currTokenType == STRING){
+                t.val = std::get<std::string>(t.val) +currChar;
+            }
+            else{
+                currTokenType = FLOAT;
+                t.type = FLOAT;
+                isPreDecimal = false;
+            }
+            break;
+        
         default:
-            data += c;
+            // std::cout<<"am i here";
+            if(currTokenType!=STRING){
+                
+                if(currTokenType != FLOAT)
+                {
+                    
+                    if(isdigit(currChar)){
+                        if(currTokenType==UNKNOWN){
+                            // std::cout<<"am i here";
+                            currTokenType = INTEGER;
+                            t.val = 0;
+                            t.type = INTEGER;
+                        }
+                        currTokenType = INTEGER;
+                        t.type = INTEGER;
+                        int x = currChar - '0';
+                        // t.val.integer = t.val.integer*10 + x;
+                        t.val = std::get<int>(t.val)*10 +x;
+                    }
+                }
+                else{
+                    
+                    if(isPreDecimal){
+                        int x = currChar - '0';
+                        // t.val.integer = t.val.integer*10 + x;
+                        
+                        t.val = std::get<int>(t.val)*10 +x;
+                    }
+                    else{
+                        float x = currChar - '0';
+                        // t.val.fl = t.val.fl + x/10;
+                        if(std::holds_alternative<int>(t.val)){
+                            t.val = static_cast<float>(std::get<int>(t.val));
+                        }
+                        t.val = std::get<float>(t.val) +x/10;
+                    }
+                }
+
+                if(currTokenType == UNKNOWN){
+                    if(currChar=='n'){
+                        mayBeNull = 1;
+                        continue;
+                    }
+                    if(currChar=='t'){
+                        mayBeTrue = 1;
+                        continue;
+                    }
+                    if(currChar=='f'){
+                        mayBeFalse = 1;
+                        continue;
+                    }
+                }
+
+            }
+            else{
+                
+                t.val = std::get<std::string>(t.val) +currChar;
+            }
+            if(currTokenType == UNKNOWN){
+                if(mayBeNull!=0){
+                    if((mayBeNull==1) && (currChar=='u')){
+                        mayBeNull++;
+                        continue;
+                    }
+                    if((mayBeNull==2) && (currChar=='l')){
+                        mayBeNull++;
+                        continue;
+                    }
+                    if((mayBeNull==3) && (currChar=='l')){
+                        mayBeNull++;
+                        t.type = NULLOBJ;
+                        return t;
+                    }
+                }
+                if(mayBeTrue!=0){
+                    if(mayBeTrue==1 && currChar=='r'){
+                        mayBeTrue++;
+                        continue;
+                    }
+                    if(mayBeTrue==2 && currChar=='u'){
+                        mayBeTrue++;
+                        continue;
+                    }
+                    if(mayBeTrue==3 && currChar=='e'){
+                        mayBeTrue++;
+                        t.type = BOOL;
+                        t.val = true;
+                        return t;
+                    }
+                }
+                if(mayBeFalse!=0){
+                    if(mayBeFalse==1 && currChar=='a'){
+                        mayBeFalse++;
+                    }
+                    if(mayBeFalse==2 && currChar=='l'){
+                        mayBeFalse++;
+                    }
+                    if(mayBeFalse==3 && currChar=='s'){
+                        mayBeFalse++;
+                    }
+                    if(mayBeFalse==4 && currChar=='e'){
+                        mayBeFalse++;
+                        t.type = BOOL;
+                        t.val = false;
+                        return t;
+                    }
+                }
+            }
             break;
         }
     }
-    return tokenList;
+    return t;
 }
 
-int main(){
-    return 0;
+void printToken(Token t){
+    std::cout<<translator[t.type]<<std::endl;
+}
+
+void parseTokens(FILE* pFile){
+    std::vector<Token> tokens;
+    char currChar;
+    while((currChar = fgetc(pFile))!=EOF){
+        ungetc(currChar, pFile);
+        Token token = getToken(pFile);
+        tokens.push_back(token);
+        printToken(token);
+    }
+    // for(auto t : tokens){
+    //     printToken(t);
+    // }
+    
 }
---
+++ tokeniser.h
@@ -0,0 +1,30 @@
+#include <variant>
+#include <string>
+
+enum TokenType{
+    OPENBRACE,
+    CLOSEDBRACE,
+    COLON,
+    STRING,
+    INTEGER,
+    FLOAT,
+    BOOL,
+    NULLOBJ,
+    DELIMITER,
+    INVALID,
+    UNKNOWN
+};
+
+
+typedef std::variant<std::string, int, float, bool> TokenVal;
+
+class Token
+{
+public:
+    TokenType type;
+    TokenVal val;
+};
+
+Token getToken(FILE* pFile);
+
+void parseTokens(FILE* pFile);
